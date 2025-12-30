49fc179fabc63b809a2be078c2ce1d15e5f214c7
Renan UFPB <renantomazini@sti.ufpb.br>
2025-12-30 07:56:29 -0300

    atualização particulas

--- cenas/teste_recurso/arrastar.gd
+++ cenas/teste_recurso/arrastar.gd
@@ -3,23 +3,22 @@ extends Area2D
 var dragging := false
 var offset := Vector2.ZERO
 
-
 @onready var escova: AnimatedSprite2D = $AnimatedSprite2D
-@onready var bolha: CPUParticles2D = $CerdasColision/Bolhas
+@onready var bolha: CPUParticles2D = $CerdasArea/CerdasColision/Bolhas
 var max_points := 100
 
 func _input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		dragging = event.pressed
 		if dragging:
-			bolha.emitting = true
+			#bolha.emitting = true
 			#bolha.set_emitting(true)
 			offset = escova.global_position - get_global_mouse_position()
 		else:
-			bolha.emitting = false
+			pass
+			#bolha.emitting = false
 			#bolha.set_emitting(false)
 			#trail.clear_points() # opcional: limpa ao soltar
-		
 
 func _process(delta):
 	if dragging:
@@ -28,8 +27,14 @@ func _process(delta):
 		#print(self.global_position)
 		#print("----")
 
-
-
 func _on_dente_area_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
 	#print(area.name)
 	pass
+
+func _on_cerdas_area_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+	if area.name == "denteArea":
+		bolha.emitting = true
+
+func _on_cerdas_area_area_shape_exited(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+	if area.name == "denteArea":
+		bolha.emitting = false
